35b43e19d8a30256920e7955508233025861d753
Rambhu1819 <156609631+Rambhu1819@users.noreply.github.com>
2026-01-26 10:19:18 +0530

    Time: 14 ms (89.89%) | Memory: 53 MB (5.55%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -1,27 +1,33 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
 
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        HashMap<Integer, Integer> first = new HashMap<>();
-        HashMap<Integer, Integer> last = new HashMap<>();
+        // value -> [firstIndex, lastIndex, frequency]
+        HashMap<Integer, int[]> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int x = nums[i];
 
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-            first.putIfAbsent(x, i);
-            last.put(x, i);
+            if (!map.containsKey(x)) {
+                map.put(x, new int[]{i, i, 1});
+            } else {
+                map.get(x)[1] = i;      // update last index
+                map.get(x)[2]++;        // increase frequency
+            }
         }
 
         int degree = 0;
         int ans = nums.length;
 
-        for (int key : freq.keySet()) {
-            if (freq.get(key) > degree) {
-                degree = freq.get(key);
-                ans = last.get(key) - first.get(key) + 1;
-            } else if (freq.get(key) == degree) {
-                ans = Math.min(ans, last.get(key) - first.get(key) + 1);
+        for (int[] data : map.values()) {
+            int first = data[0];
+            int last = data[1];
+            int freq = data[2];
+
+            if (freq > degree) {
+                degree = freq;
+                ans = last - first + 1;
+            } else if (freq == degree) {
+                ans = Math.min(ans, last - first + 1);
             }
         }
 
